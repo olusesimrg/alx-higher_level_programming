213ce808681636d9943bc9d30ed6099403a74fe2
Olusesi Saheed Gbolahan <olusesimrgees@gmail.com>
2023-04-05 05:15:15 -0700

    task say myname

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
+
+"""Prototype: def say_my_name(first_name, last_name=""):
+    both the argument must be str
+    one of the argument much be a str
+    raise alarm if not str
 """
 
-"""
 
 def say_my_name(first_name, last_name=""):
-    """
+    """set first and last name 
+        both the argument must be a str
         Raises:
         TypeError: if either argument is not a string
     """
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
-    if type(last_name) != str:
+    elif type(last_name) != str:
         raise TypeError('last_name must be a string')
-    if not(last_name):
+    elif not(last_name):
         print("My name is {} ".format(first_name))
     else:
         print("My name is {} {}".format(first_name, last_name))
